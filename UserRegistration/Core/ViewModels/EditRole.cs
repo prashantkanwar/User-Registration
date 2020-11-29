f26208ed31b78f385f58b845522ac2cd75e5c2df
Prashant Kanwar <prashant.kanwar2015@gmail.com>
2020-11-29 17:33:21 +0545

    Initial commit

---
+++ UserRegistration/Core/ViewModels/EditRole.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Core.ViewModels
+{
+    public class EditRole
+    {
+        public string Id { get; set; }
+        [Required(ErrorMessage ="Role Name is required")]
+        public string RoleName { get; set; }
+
+        public List<string> Users { get; set; }
+
+    }
+}
---
+++ UserRegistration/UserRegistration.UI/Controllers/AdministrationController.cs
@@ -0,0 +1,151 @@
+﻿using Core.Interfaces;
+using Core.Models;
+using Core.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserRegistration.UI.Controllers
+{
+    [Authorize(Roles ="Admin")]
+    public class AdministrationController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IAdministrationServices _administrationServices;
+        public AdministrationController(IAdministrationServices administrationServices, RoleManager<IdentityRole> roleManager,
+                                                        UserManager<ApplicationUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+            _administrationServices = administrationServices;
+        }
+
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var result = await _userManager.DeleteAsync(user);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("ListUsers");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+            return View("ListUsers");
+        }
+    }
+
+    [HttpGet]
+        public async Task<IActionResult> ListUsers()
+        {
+            var users = await _userManager.Users.ToListAsync();
+           return View(users);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditUser(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var model = new EditUser
+            {
+                Id = user.Id,
+                Email = user.Email,
+                UserName = user.UserName,
+                FirstName = user.FirstName,
+                LastName=user.LastName
+
+            };
+
+            return View(user);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> EditUser(EditUser editUser)
+        {
+            var user = await _userManager.FindByIdAsync(editUser.Id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            else
+            {
+                user.Email = editUser.Email;
+                user.UserName = editUser.UserName;
+                user.FirstName = editUser.FirstName;
+                user.LastName = editUser.LastName;
+                
+
+                var result = await _userManager.UpdateAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListUsers");
+                }
+
+                foreach (var error in result.Errors) 
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View(editUser);
+            }
+        }
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+   
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRole createRole)
+        {
+            if(ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole
+                {
+                    Name = createRole.RoleName
+                };
+                IdentityResult result = await _roleManager.CreateAsync(identityRole);
+                
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles", "Administration");
+                }
+                foreach(IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(createRole);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ListRoles()
+        {
+            var roles = await _roleManager.Roles.ToListAsync();
+            return View(roles);
+        }
+    }
+}
